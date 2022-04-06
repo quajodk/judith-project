5a2cbe12ac51f6472600da368260ed4eec94dbc3
Quajo Duke <quajoduke305@gmail.com>
2022-04-06 08:50:14 +0000

    clear console

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -25,8 +25,6 @@ export default function ProductList() {
     }
   }, [products.length]);
 
-  console.log(total, products.length, lastDocRef);
-
   if (products.length === 0 && fetchingProduct) {
     return (
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
