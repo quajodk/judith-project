c388b6b109d0e64e1c3296995ab1f9eb912af938
Quajo Duke <quajoduke305@gmail.com>
2022-05-16 07:58:21 +0000

    added the other image

--- src/components/HeroBg.tsx
+++ src/components/HeroBg.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Slider from "react-slick";
 import SliderImg from "../assets/images/slider.jpeg";
+import Slider1Img from "../assets/images/slider1.jpeg";
 
 interface Props {}
 
@@ -11,14 +11,27 @@ const HeroBg = (props: Props) => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
   };
   return (
     <Slider {...settings}>
       <div aria-hidden="true" className="sm:hidden">
         <div className="flex-1 relative w-full bg-gray-800 opacity-50">
-          <div className="inset-0 overflow-hidden" style={{ height: 450 }}>
+          <div className="inset-0 overflow-hidden" style={{ height: 500 }}>
             <img
               src={SliderImg}
+              alt=""
+              className="w-full h-full object-center object-cover"
+            />
+          </div>
+          <div className="inset-0 bg-gray-900 opacity-50" />
+        </div>
+      </div>
+      <div aria-hidden="true" className="sm:hidden">
+        <div className="flex-1 relative w-full bg-gray-800 opacity-50">
+          <div className="inset-0 overflow-hidden" style={{ height: 500 }}>
+            <img
+              src={Slider1Img}
               alt=""
               className="w-full h-full object-center object-cover"
             />
